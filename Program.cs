35c8b32a5dccf3a9c93f9c46c499657c67da55c9
lfrosa <lfrosa@outlook.com>
2025-03-20 17:41:48 -0300

    retirando validação

--- Program.cs
+++ Program.cs
@@ -5,19 +5,19 @@ using RiskDev.Services;
 
 
 
-var service = new RiskCategorizationService();
+// var service = new RiskCategorizationService();
 
-DateTime database = new DateTime(2020, 12, 11);
+// DateTime database = new DateTime(2020, 12, 11);
 
-Console.WriteLine(Enum.GetName(service.Evaluate(new Trade(2000000, "Private", new DateTime(2025, 12, 29)), database)));
-Console.WriteLine(Enum.GetName(service.Evaluate(new Trade(400000, "public", new DateTime(2020, 7, 01)), database)));
-Console.WriteLine(Enum.GetName(service.Evaluate(new Trade(5000000, "public", new DateTime(2024, 1, 2)), database)));
-Console.WriteLine(Enum.GetName(service.Evaluate(new Trade(3000000, "public", new DateTime(2023, 10, 26)), database)));
-
-
+// Console.WriteLine(Enum.GetName(service.Evaluate(new Trade(2000000, "Private", new DateTime(2025, 12, 29)), database)));
+// Console.WriteLine(Enum.GetName(service.Evaluate(new Trade(400000, "public", new DateTime(2020, 7, 01)), database)));
+// Console.WriteLine(Enum.GetName(service.Evaluate(new Trade(5000000, "public", new DateTime(2024, 1, 2)), database)));
+// Console.WriteLine(Enum.GetName(service.Evaluate(new Trade(3000000, "public", new DateTime(2023, 10, 26)), database)));
 
 
-//new Start().Run();
+
+
+new Start().Run();
 
 
 
--- Services/Start.cs
+++ Services/Start.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Runtime.ConstrainedExecution;
-using System.Threading.Tasks;
 using RiskDev.Models;
-
 namespace RiskDev.Services;
 
 public class Start
